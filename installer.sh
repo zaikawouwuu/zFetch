171d41d39a035eee31edc0468932359a7c1dccc1
zaikawouwuu <120923516+zaikawouwuu@users.noreply.github.com>
2023-02-26 14:08:52 +0000

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "Downloading index file..."
-curl -sSL "https://github.com/zaikawouwuu/zfetch/raw/main/index.js" -o "file.js"
+curl -sSL "https://github.com/zaikawouwuu/zFetch/raw/main/index.js" -o "file.js"
 echo "Formatting index file..."
 fold -w 160 -s file.js > formatted_file.js
 rm file.js
-mv formatted_file.js file.js
+mv formatted_file.js index.js
 echo "-> Index file formatting complete."
 echo "Starting npm..."
 npm init -y
@@ -15,5 +15,13 @@ mkdir cfg
 echo "arch = 23 147 209;
 debian = 215 10 83;" >> ./cfg/colors.txt
 mkdir logos
-echo "debian logo here" >> ./logos/debian_logo.txt
-echo "arch logo here" >> ./logos/arch_logo.txt
+curl -sSL "https://github.com/zaikawouwuu/zFetch/raw/main/debian_logo.txt" -o "./logos/debian_logo.txt"
+curl -sSL "https://github.com/zaikawouwuu/zFetch/raw/main/arch_logo.txt" -o  "./logos/arch_logo.txt"
+cd ~/.config
+mkdir zfetch
+cd zfetch
+touch zfetch.conf
+echo "DISPLAYED_LOGO = auto;
+LOGO_COLOR = auto;
+GET_INFO = auto;
+" >> zfetch.conf
